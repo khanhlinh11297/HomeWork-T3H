6168d61f9b83ace5cfe2eaae2bffee74d850d83b
linh nguyen <khanhlinh11297@gmail.com>
2020-11-01 21:52:47 +0700

    Fix 1

--- main.js
+++ main.js
@@ -27,13 +27,13 @@ Má Tèo muốn sắp xếp lại gọn gàng, đơn giản, dễ nhìn như ví
  */
 const data = [1, 2, [3, 4, [5]]]
 // Way 1
-const flatten = arr => {
+const flatten = array => {
   const convertToString = arr.toString().replaceAll(',', '')
   return convertToString.split('')
 }
 
 // Way 2:
-const flat = arr => {
+const flat = array => {
   const convertToString = arr.flat(Infinity)
   return convertToString
 }
@@ -46,118 +46,22 @@ Kệ data chínhh là công sức lần trước của Tèo, việc bây giờ T
 Mỗi 1 kệ sẽ chứa tối đa 1 hay 2 hay 3 hay ... bao nhiêu tùy Tèo cho phép, vì quá mệt mỏi, Tèo đã chạy sang nhờ bạn code hộ output bên dưới
  */
 const data2 = [1, 2, 3, 4, 5, 6, 7,8];
-
-const divideArray = (arr,n) => {
-  const tempArray = []
-  for (let i = 0; i < arr.length; i += n) {
-    myChunk = arr.slice(i, i + n)
-    tempArray.push(myChunk);
+const chunk = (array,n) => {
+  const newArray = []
+  for (let i = 0; i < array.length; i += n) {
+    myChunk = array.slice(i, i + n)
+    newArray.push(myChunk);
   }
-  return tempArray
+  return newArray
 }
 
-console.log(divideArray(data2,3));
-console.log("Bài 3 :");
-const data_3 = [1, 2, 3, 4, 5, 6, 7,8];
-var newdata_3 = [],
-    newarr_3 = [];
-
-function chunk(data_3, n) {
-    for (var i = 0; i < data_3.length; i++) {
-        newarr_3.push(data_3[i]);
-        if ((i + 1) % n === 0) {
-            newdata_3.push(newarr_3);
-            newarr_3 = [];
-        } 
-        else if (i >= (Math.floor(data_3.length / n)) * n) {
-            newdata_3.push(newarr_3);
-            newarr_3 = [];
-        }
-    }
-}
-
-chunk(data_3, 3);
-console.log(newdata_3);
-
-// Exercise 4:
-const findDuplicatesArray = array => {
-  const duplicatesArray = []
-  const uniqueArray = []
-  for (let i = 0; i < array.length; i++) {
-    if (uniqueArray.indexOf(array[i]) === -1) {
-      uniqueArray.push(array[i])
-    } else {
-      duplicatesArray.push(array[i])
-    }
-  }
-  return duplicatesArray
-}
-
-const intersectionArray = (array1, array2, array3 = []) => {
-  const unifyArray = [ ...array1, ...array2, ...array3 ]
-  return findDuplicatesArray(unifyArray)
-}
-
-//---
-const getIntersection = (data1, data2) => {
-  const result = []
-  for (let i = 0; i < data1.length; i++) {
-    for (let j = 0; j < data2.length; j++) {
-      if (data1[i] === data2[j]) {
-        result.push(data1[i])
-      }
-    }
-  }
-  return result
-}
-
-const arr11 = [1, 2,4,5,6,76]
-const arr22 = [2, 2,2,2,3,76,3,9]
-
-const duplicate = []
-let check = false
-for (let i = 0; i < arr11.length; i++) {
-  for (let j = 0; j < arr22.length; j++) {
-    if (arr11[i] === arr22[j]) {
-      for (let r = 0; r < duplicate.length; r++) {
-        if (arr11[i] === duplicate[r]) {
-          check = true
-        }
-      }
-      if (check == false) {
-        duplicate.push(arr11[i])
-        check = false
-      }
-    }
-  }
-} console.log('số trùng:', duplicate); 
-
-//----------------- 
+/**
+ * Lesson 4:
+Không sống nổi với má, Tèo quyết định đi làm ở 1 công ty ở Thạch Thất, và đang phải phân tích dựa vào các data đã có sẵn, mỗi 1 array là thông tin của 1 cái gì đó mà Tèo cũng không biết nữa, nhiệm vụ của Tèo là tìm ra điểm chung của 2 array với nhau, Tèo đang khóc ra tiếng mán vì quá khó, hãy giúp Tèo
+ */
 // ex5:
 const checkDates = (date) => {
-  const milestoneTime = 'Oct 30, 2020 12:30:32'
-  if (Date.parse(date) <= Date.parse(milestoneTime)) return true
+  const timeSale = 'Oct 30, 2020 12:30:32'
+  if (Date.parse(date) <= Date.parse(timeSale)) return true
   else return false
 }
-
-const kiemtra = (bien2) => {
-  const bien1 = '12'
-  if (bien2 <= bien1) return true
-  else return false
-}
-
-
-
-//ex 6:
-const checkUserName = (userName) => {
-  const regexUserName = /^([a-z0-9_]){2,10}$/
-  return userName.indexOf('__') === -1 ? regexUserName.test(userName) : false
-}
-const checkEmail = (email) => {
-  const regexEmail = /^[a-z0-9]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z](?:[a-z]*[a-z])?\.)+[a-z](?:[a-z]*[a-z])?$/i
-  return regexEmail.test(email)
-}
-//----------------- 
-
-
-
